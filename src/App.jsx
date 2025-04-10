a9116ee8a42dab38a23b516e3431f5673410bd9e
Ethan <chenstyle2022@outlook.com>
2025-04-10 19:21:34 +0800

    finish About and Work section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import './App.css'
 import './style/button.css'
 import AnimatedContent from './components/AnimatedContent'
 
+import ArrowIcon from './svgs/ArrorIcon'
+import GithubIcon from './svgs/GithubIcon'
+import PostIcon from './svgs/PostIcon'
+import ShinyText from './components/ShinyText'
+
+import todoPic from './pic/todolist-proj.png'
+
 function App() {
   const [lang, setLang] = useState('en')
 
@@ -50,8 +57,8 @@ function App() {
             <p className='home-smallHeader'>Hi, my name is</p>
             <p className='home-name'>Ethan Wang</p>
             <p className='home-role'>I'm a Software Engineer</p>
-            <p className='home-brief'>I have a strong passion for software engineering with experience building{' '} 
-                                      <span style={{color: 'var(--main-color-light)'}}>Fullstack</span>{' '}
+            <p className='home-brief text-hover'>I have a strong passion for software engineering with experience building{' '} 
+                                      <span style={{color: 'var(--main-color-light)'}}>Full Stack</span>{' '}
                                        web applications and applying {' '}
                                       <span style={{color: 'var(--main-color-light)'}}>Deep Learning</span>{' '}
                                        to solve real-world problems. </p>
@@ -61,13 +68,130 @@ function App() {
       </div>
       <div className='about'>
         <div className='about-box'>
+        <AnimatedContent
+            distance={150}
+            direction="horizontal"
+            reverse={true}
+            config={{ tension: 50, friction: 20 }}
+            initialOpacity={0}
+            animateOpacity
+            scale={1}
+            threshold={0.1}
+          >
           <div className='about-left'>
             <div className='about-me'>About Me</div>
-            <p className='about-para'>Hi there! My name is Ethan and I love creating useful web applications that could help others and myself.</p>
-            <p className='about-para'>My interest in web development began with a front-end development course I took at UNSW in 2024, where I learned to build my own website and develop productivity tools.</p>
-            <p className='about-para'>Today, I have graduated from the University of New South Wales Master of IT and worked as a full stack engineer at RED BLACK TREE TECHNOLOGIES PTY LTD!</p>
+            <p className='about-para text-hover'>Hi there! My name is Ethan and I love creating useful web applications that could help others and myself.</p>
+            <p className='about-para text-hover'>My interest in {' '}<span style={{color: 'var(--main-color-light)'}}>web development</span>{' '}  began with a front-end development course I took at UNSW in 2024, where I learned to build my own website and develop productivity tools.</p>
+            <p className='about-para text-hover'>Today, I have graduated from the University of New South Wales Master of IT and worked as a full stack engineer at RED BLACK TREE TECHNOLOGIES PTY LTD!</p>
+            <p className='about-para text-hover'>Here are a few technologies I've been working with recently!</p>
+            <div className='about-skill'>
+              <div className='about-skill-row'>
+                <div style={{display:"flex", flexDirection:"row", alignItems:"center", gap:'5px', width:'100px'}}><ArrowIcon/><p>JavaScript</p></div>
+                <div style={{display:"flex", flexDirection:"row", alignItems:"center", gap:'5px', width:'100px'}}><ArrowIcon/><p>React.js</p></div>
+              </div>
+              <div className='about-skill-row'>
+                <div style={{display:"flex", flexDirection:"row", alignItems:"center", gap:'5px', width:'100px'}}><ArrowIcon/><p>Node.js</p></div>
+                <div style={{display:"flex", flexDirection:"row", alignItems:"center", gap:'5px', width:'100px'}}><ArrowIcon/><p>PostgreSQL</p></div>
+              </div>
+              <div className='about-skill-row'>
+                <div style={{display:"flex", flexDirection:"row", alignItems:"center", gap:'5px', width:'100px'}}><ArrowIcon/><p>PyTorch</p></div>
+                <div style={{display:"flex", flexDirection:"row", alignItems:"center", gap:'5px', width:'100px'}}><ArrowIcon/><p>CNN</p></div>
+              </div>
+            </div>
           </div>
+          </AnimatedContent>
           <div className='about-right'></div>
+        </div>
+      </div>
+      <div className='work'>
+        <AnimatedContent
+          distance={150}
+          direction="vertical"
+          reverse={false}
+          config={{ tension: 50, friction: 20 }}
+          initialOpacity={0}
+          animateOpacity
+          scale={1}
+          threshold={0.1}
+        >
+        <div className='work-box'>
+          <div className='work-header'>Where I've Worked</div>
+          {/* <div className='work-compName'>RED BLACK TREE TECHNOLOGIES PTY LTD</div> */}
+          <ShinyText text="RED BLACK TREE TECHNOLOGIES PTY LTD" disabled={false} speed={2.5} className='work-compName' />
+          <div className='work-contentBox'>
+            <div className='work-role text-hover'>Full Stack Engineer</div>
+            <p className='work-time text-hover'>Nov 2024 - Mar 2025</p>
+            <div className='work-content' style={{marginTop: '20px'}}>
+              <div style={{width:'30px', paddingRight: '20px', paddingTop:'5px'}}>
+                <ArrowIcon/>
+              </div>
+              <p className='work-content-text text-hover'>Independently designed and developed a fully functional, 
+                                        performance-optimized modern online forum system, RBT Forum, 
+                                        covering the full project lifecycle from requirements analysis, 
+                                        system architecture design, 
+                                        front- and back-end development.</p>
+            </div>
+            <div className='work-content' style={{marginTop: '20px'}}>
+              <div style={{width:'30px', paddingRight: '20px', paddingTop:'5px'}}>
+                <ArrowIcon/>
+              </div>
+              <p className='work-content-text text-hover'>Ensured a clean project structure and high code quality with strong emphasis on maintainability and scalability.</p>
+            </div>
+            <div className='work-content' style={{marginTop: '20px'}}>
+              <div style={{width:'30px', paddingRight: '20px', paddingTop:'5px'}}>
+                <ArrowIcon/>
+              </div>
+              <p className='work-content-text text-hover'>Achieved excellent user experience through meticulous performance optimization.</p>
+            </div>
+    
+          </div>
+        </div>
+        </AnimatedContent>
+      </div>
+      <div className='project'>
+        <div className='project-box'>
+          <div className='project-header'>My Noteworthy Projects</div>
+          <div className="project-container" style={{marginTop: '60px'}}>
+            <a
+              href="https://chen-style.github.io/todolist-deploy/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="project-left-pic"
+            >
+              <img src={todoPic} alt="todo" />
+            </a>
+            <div className="project-right-info">
+              <p className='project-right-header'>Featured Project</p>
+              <p className='project-right-title'>Todo List</p>
+              <div className='project-right-card'>
+                <p className="description">Netflix is a subscription-based streaming service ...</p>
+              </div>
+              <div className='project-right-skillset'>
+                <p className='project-skill'>React</p>
+                <p className='project-skill'>Vite</p>
+                <p className='project-skill'>HTML</p>
+                <p className='project-skill'>CSS</p>
+              </div>
+              <div className='project-right-iconBox'>
+                <a
+                  href="https://github.com/CHEN-Style/TodoList"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className='link-hover'
+                >
+                  <GithubIcon/> 
+                </a>
+                <a
+                  href="https://github.com/CHEN-Style/TodoList"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className='link-hover'
+                >
+                  <PostIcon/>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
---
+++ src/svgs/ArrorIcon.jsx
@@ -0,0 +1,16 @@
+const ArrowIcon = ({ width = 16, height = 16, fill = '#64ffdbd3' }) => (
+  <svg
+    className="icon"
+    viewBox="0 0 1024 1024"
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+  >
+    <path
+      d="M562.005333 512l-211.2-211.2 60.330667-60.288L682.666667 512l-271.530667 271.530667-60.330667-60.373334 211.2-211.2z"
+      fill={fill}
+    />
+  </svg>
+);
+
+export default ArrowIcon;
